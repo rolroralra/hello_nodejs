acffcf5f4aa08ec19d1ccb1d4279ed18d6bffe47
rolroralra <rolroralra@gmail.com>
2020-02-10 11:20:37 +0900

    Using http,fs Module, Run Http Server

--- hello/helloserver.js
+++ hello/helloserver.js
@@ -1,10 +1,35 @@
 const http = require('http');
+const fs = require('fs');
 const httpserver = http.createServer(function(req, res) {
   // client의 요청 정보(req)를 분석한 뒤, 응답 메시지(res)를 전송
-  res.writeHead(200);
-  res.end('<h1>Hello HTTP Server</h1>');
+  // res.writeHead(200);
+  // res.end('<h1>Hello HTTP Server</h1>');
+  console.log(req.method, req.url, req.httpVersion);
+  console.log(req.headers);
+
+  var filename = req.url.substring(1);
+  // 1. 동기화 방식 함수 호출
+  // try {
+  //   var data = fs.readFileSync(filename);
+  //   res.writeHead(200);
+  //   res.end(data);
+  // } catch (err) {
+  //   res.writeHead(404);
+  //   res.end('<h1>' + filename + ' Not Found!</h1>');
+  // }
+
+  // 2. 비동기 방식 함수 호출
+  fs.readFile(filename, function(err, data){
+    if (err) {
+      res.writeHead(404);
+      res.end('<h1>' + filename + ' Not Found!</h1>');
+    } else {
+      res.writeHead(200);
+      res.end(data);
+    }
+  });
 });
 
-httpserver.listen(8080, function() {
+httpserver.listen(80, function() {
   console.log('Server Init Complete!');
 });
