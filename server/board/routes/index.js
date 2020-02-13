a0c2ebbe8c1aa1ab0f248332f2f1a08ea9a07626
rolroralra <rolroralra@gmail.com>
2020-02-13 15:01:13 +0900

    Express Initialization

--- server/board/routes/index.js
+++ server/board/routes/index.js
@@ -3,7 +3,35 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+  res.redirect('/board');
+});
+
+// 목록 조회
+router.get('/board', function(req, res, next) {
+  res.render('board/list', { title: 'Express' });
+});
+
+// 등록 화면 요청
+router.get('/board/new', function(req, res, next) {
+  res.render('board/write', { title: 'Express' });
+});
+
+// 등록 처리 요청
+router.post('/board/new', function(req, res, next) {
+  res.render('board/result', { title: 'Express' });
+});
+
+// 상세 조회
+router.get('/board/:no', function(req, res, next) {
+  var no = req.params.no;
+  res.render('board/view', { title: 'Express' });
+});
+
+// 삭제
+router.post('/board/:no', function(req, res, next) {
+  var no = req.params.no;
+  res.render('board/list', { title: 'Express' });
 });
 
 module.exports = router;
